892bf83d25fcbd392f6657452cecc267de4a51cc
JACarrasco <jacarrascogonzalez@gmail.com>
2026-06-29 20:15:32 +0200

    feat: Pest Dusk browser tests scaffold

---
+++ tests/Browser/Vehicle/VehicleBrowserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use App\Models\User;
+use App\Modules\Identity\Models\Garage;
+use App\Modules\Vehicle\Models\Vehicle;
+
+uses(DuskTestCase::class);
+
+test('user can create vehicle via browser', function () {
+    $user = User::factory()->create();
+    $garage = Garage::factory()->create(['user_id' => $user->id]);
+
+    $this->browse(function (Browser $browser) use ($user, $garage) {
+        $browser->loginAs($user)
+            ->visit(route('vehicles.create'))
+            ->type('#plate', 'TEST123')
+            ->type('#brand', 'Seat')
+            ->type('#model', 'León')
+            ->type('#year', '2020')
+            ->select('#fuel_type', 'gasolina')
+            ->press('Guardar')
+            ->assertPathIs(route('vehicles.index', absolute: false))
+            ->waitForLivewire()
+            ->assertSee('Seat León');
+    });
+});
+
+test('user can view vehicle details', function () {
+    $user = User::factory()->create();
+    $garage = Garage::factory()->create(['user_id' => $user->id]);
+    $vehicle = Vehicle::factory()->create(['garage_id' => $garage->id, 'brand' => 'Toyota', 'model' => 'Corolla']);
+
+    $this->browse(function (Browser $browser) use ($user, $vehicle) {
+        $browser->loginAs($user)
+            ->visit(route('vehicles.show', $vehicle))
+            ->assertSee('Toyota Corolla')
+            ->assertSee($vehicle->plate);
+    });
+});
